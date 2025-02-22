9911a9f3ead36c83b2c9babef8e2a5e598eb2a08
khushal1512 <khushal080424@gmail.com>
2025-02-23 00:19:22 +0530

    type error fix

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -9,7 +9,7 @@ import Link from "next/link"
 import React from "react"
 
 
-const questions: QuestionProps[] = [
+const questions = [
   { 
     _id: "1", 
     title: "Cascading Deletes in SQLAlchemy?", 
