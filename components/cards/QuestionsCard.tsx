55708ee2d6a5f9cbd203718feaec29a3feee6ce6
khushal1512 <khushal080424@gmail.com>
2025-02-23 00:45:00 +0530

    Metrics and Questions Card Component

--- components/cards/QuestionsCard.tsx
+++ components/cards/QuestionsCard.tsx
@@ -1,5 +1,8 @@
+import Link from 'next/link';
 import { title } from 'process';
 import React from 'react'
+import RenderTag from '../shared/RenderTag';
+import Metrics from '../shared/Metrics';
 
 
 interface QuestionProps {
@@ -19,9 +22,38 @@ interface QuestionProps {
     answers: Array<object>;
     createdAt: Date;
 }
+
 const QuestionsCard = ({ _id, title, tags, author, views, upvotes, answers, createdAt} : QuestionProps) => {
   return (
-    <div>QuestionsCard</div>
+    <div className='card-wrapper p-9 sm:rounded-[10px] sm:px-11'>
+        <div className="flex flex-col-reverse items-start justify-between gap-5 sm:flex-row">
+            <div>
+                <span className='subtle-regular text-dark400_light700 line-clamp-1 flex sm:hidden'>
+                    {String(createdAt)}
+                </span>
+                <Link href={`/questions/${_id}`}>
+                    <h3 className='sm:h3-semibold base-semibold text-dark200_light900 line-clamp-1 flex-1'>
+                        {title}
+                    </h3>
+                </Link>
+            </div>
+
+            {/* {if signed in add edit delete action} */}
+        </div>
+        
+        <div className="mt-3.5 flex flex-wrap gap-2">
+            {tags.map((tag) => (
+                <RenderTag key={tag._id}
+                _id={tag._id}
+                name={tag.name}
+                />
+            ))}
+        </div>
+
+        <div className="flex-between mt-6 w-full flex-wrap gap-3">
+            <Metrics />
+        </div>
+    </div>
   )
 }
 
