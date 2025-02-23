2ebfff429bff6f40d4db32536866400f76ecf76b
khushal1512 <khushal080424@gmail.com>
2025-02-23 09:54:25 +0530

    Card Component and Metrics Styling

--- components/shared/Metrics.tsx
+++ components/shared/Metrics.tsx
@@ -1,8 +1,27 @@
+import Image from 'next/image';
 import React from 'react'
 
-const Metrics = () => {
+
+interface MetricProps { 
+  imgUrl: string; 
+  alt: string; 
+  value: string | number; 
+  title: string; 
+  href?: string; 
+  isAuthor?: string;
+  textStyles?:string; 
+}
+
+const Metrics = ({ imgUrl, alt, value, title, href, textStyles, isAuthor} : MetricProps ) => {
   return (
-    <div>Metrics</div>
+    <div className='flex-center flex-wrap gap-1'>
+      <Image
+        src={imgUrl}
+        width={16}
+        height={16}
+        alt={alt}
+        className={`object-contain ${href ? 'rounded-full' : ''}`}
+    </div>
   )
 }
 
