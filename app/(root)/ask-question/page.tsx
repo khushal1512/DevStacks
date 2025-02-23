cc8c4c01183d320339e0261d72bc2db3c63d8dd3
khushal1512 <khushal080424@gmail.com>
2025-02-23 21:10:29 +0530

    Questions Form Validation

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -4,10 +4,10 @@ import React from 'react'
 const page = () => {
   return (
     <div>
-      <h1 className=''>
-
+      <h1 className='h1-bold text-dark100_light900'>
+        Ask a Question
       </h1>
-      <div>
+      <div className='mt-9'>
         <Question />
       </div>
     </div>
