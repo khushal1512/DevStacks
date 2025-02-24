d809da06775ade87d2542424388a5fb4f15abfd3
khushal1512 <khushal080424@gmail.com>
2025-02-25 00:48:00 +0530

    Styling the form

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -21,32 +21,82 @@ const Question = () => {
     resolver: zodResolver(QuestionsSchema),
     defaultValues: {
       title: "",
-      explanatin: "",
+      explanation: "",
       tags: [],
     },
   });
 
- 
   function onSubmit(values: z.infer<typeof QuestionsSchema>) {
     console.log(values);
   }
 
   return (
     <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="flex w-full flex-col gap-10">
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className="flex w-full flex-col gap-10"
+      >
         <FormField
           control={form.control}
           name="title"
           render={({ field }) => (
             <FormItem className="flex w-full flex-col">
-              <FormLabel className="paragrph-seminold text-dark400_light800">Question Title <span className="text-primary-500">*</span></FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
+              <FormLabel className="paragrph-seminold text-dark400_light800">
+                Question Title <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className="mt-3.5">
+                <Input
+                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 test-dark300_light700 m-h-[56px] border"
+                  {...field}
+                />
               </FormControl>
-              <FormDescription>
-                This is your public display name.
+              <FormDescription className="body-regular text mt-2.5 text-light-500">
+                Be specific and imagine you&apos;re asking a question to another
+                person.
               </FormDescription>
-              <FormMessage />
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="explanation"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col gap-3">
+              <FormLabel className="paragrph-seminold text-dark400_light800">
+                Detailed Explanation of your problem <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className="mt-3.5">
+                {/* {TEXT EDITOR COMPONENT} */}
+              </FormControl>
+              <FormDescription className="body-regular text mt-2.5 text-light-500">
+                Introduce the problem and expand on what you put in the tile. Minimum 20 characters.
+              </FormDescription>
+              <FormMessage className="text-red-500" />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="tags"
+          render={({ field }) => (
+            <FormItem className="flex w-full flex-col">
+              <FormLabel className="paragrph-seminold text-dark400_light800">
+                Tags <span className="text-primary-500">*</span>
+              </FormLabel>
+              <FormControl className="mt-3.5">
+                <Input
+                  className="no-focus paragraph-regular background-light900_dark300 light-border-2 test-dark300_light700 m-h-[56px] border" placeholder="Add Tags..."
+                  {...field}
+                />
+              </FormControl>
+              <FormDescription className="body-regular text mt-2.5 text-light-500">
+                Add upto 3 tags to describe what your question 
+                is about. You need to press enter to add a tag
+              </FormDescription>
+              <FormMessage className="text-red-500" />
             </FormItem>
           )}
         />
