d3631b2d607cbbcdcf1222493bc9395d710688fc
khushal1512 <khushal080424@gmail.com>
2025-02-24 23:13:17 +0530

    Form

--- components/forms/Question.tsx
+++ components/forms/Question.tsx
@@ -30,10 +30,8 @@ const Question = () => {
     },
   });
 
-  // 2. Define a submit handler.
+ 
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
     console.log(values);
   }
 
