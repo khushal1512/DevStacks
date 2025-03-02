0dcb13e5db8423ca0c4146a0ec768d81c89046bf
khushal1512 <khushal080424@gmail.com>
2025-03-02 10:59:45 +0530

    Question, Tag and User Relation

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -1,12 +1,33 @@
-"use server"
+"use server";
 
-import { connectToDatabase } from "../mongoose"
+import Question from "@/database/question.model";
+import { connectToDatabase } from "../mongoose";
+import Tag from "@/database/tag.model";
 
+export async function createQuestion(params: any) {
+  try {
+    connectToDatabase();
 
-export async function createQuestion(params) {
-    try {
-        connectToDatabase();
-    } catch (error) {
-        
+    const { title, content, tags, author, path } = params;
+
+    // create question
+    const question = await Question.create({
+      title,
+      content,
+      author,
+    });
+
+    const tagDocuments = [];
+
+    for (const tag of tags) {
+      const existingTag = await Tag.findOneAndUpdate(
+        { name: { $regex: new RegExp(`^${tag}$`, "i") } },
+        { $setOnInsert: { name: tag }, $push: { question: question._id } },
+        { upsert: true, new: true }
+      );
+      tagDocuments.push(existingTag._id, {
+        $push: { tags: { $each: tagDocuments } },
+      });
     }
+  } catch (error) {}
 }
