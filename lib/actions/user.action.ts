19081aa33d8893593c28d7a1f6e6bf0fd4a34c33
khushal1512 <khushal080424@gmail.com>
2025-03-08 21:41:40 +0530

    user actions for clerk webhook

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,17 +1,48 @@
-"use server"
+"use server";
 
-import { connectToDatabase } from "../mongoose"
+import { connectToDatabase } from "../mongoose";
 import User from "@/database/user.model";
-export async function getUserById(params:any) {
-    try {
-        connectToDatabase(); 
-        const { userId} = params; 
+import { CreateUserParams, UpdateUserParams } from "./shared.types";
+import { revalidatePath } from "next/cache";
 
-        const user = await User.findOne({clerkId: userId}); 
+export async function getUserById(params: any) {
+  try {
+    connectToDatabase();
+    const { userId } = params;
 
-        return user; 
-    } catch(error) {
-        console.log(error); 
-        throw error;
-    }
+    const user = await User.findOne({ clerkId: userId });
+
+    return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectToDatabase();
+    const newUser = await User.create(userData);
+
+    return newUser;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    connectToDatabase();
+
+    const { clerkId, updateData, path } = params;
+    await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
