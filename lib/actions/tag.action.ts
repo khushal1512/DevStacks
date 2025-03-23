bbbc52e453d52de744b2c643d71f005d220a4f16
khushal1512 <khushal080424@gmail.com>
2025-03-23 11:48:37 +0530

    Tag ID Component for viewing questions By Tags

--- lib/actions/tag.action.ts
+++ lib/actions/tag.action.ts
@@ -37,3 +37,63 @@ export async function getTopInteractedTags(params: GetTopInteractedTagsParams) {
       throw error;
     }
   }
+
+  export async function getTagById(params: GetTagByIdParams) {
+    try {
+      connectToDatabase();
+  
+      const { tagId } = params;
+  
+      const tag = await Tag.findOne({
+        _id: tagId,
+      });
+  
+      return tag;
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  export async function getQuestionsByTagId(params: GetQuestionByTagIdParams) {
+    try {
+      connectToDatabase();
+  
+      const { tagId, page = 1, pageSize = 10, searchQuery } = params;
+  
+      // Calculate the number of questions to skip based on the page number and page size
+      const skipAmount = (page - 1) * pageSize;
+  
+      const tagFilter: FilterQuery<typeof Tag> = { _id: tagId };
+  
+      const tag = await Tag.findOne(tagFilter).populate({
+        path: "questions",
+        model: Question,
+        match: searchQuery
+          ? { title: { $regex: searchQuery, $options: "i" } }
+          : {},
+        options: {
+          sort: { createdAt: -1 },
+          skip: skipAmount,
+          limit: pageSize + 1, // +1 to check if there is next page
+        },
+        populate: [
+          { path: "tags", model: Tag, select: "_id name" },
+          { path: "author", model: User, select: "_id clerkId name picture" },
+        ],
+      });
+  
+      if (!tag) {
+        throw new Error("Tag not found");
+      }
+  
+      const questions = tag.questions;
+  
+      const isNext = tag.questions.length > pageSize;
+  
+      return { tagTitle: tag.name, questions, isNext };
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
