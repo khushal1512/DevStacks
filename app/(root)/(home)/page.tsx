349265317ea2ad9b02f4cbf7197b9412b7dc21c0
khushal1512 <khushal080424@gmail.com>
2025-03-31 16:57:35 +0530

    LocalSearch

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,73 +1,80 @@
-import QuestionsCard from "@/components/cards/QuestionsCard"
-import HomeFilters from "@/components/home/HomeFilters"
-import Filter from "@/components/shared/Filter"
-import NoResult from "@/components/shared/NoResult"
-import LocalSearchbar from "@/components/shared/search/LocalSearchbar"
-import { Button } from "@/components/ui/button"
-import { HomePageFilters } from "@/constants/filters"
-import { getQuestions } from "@/lib/actions/question.action"
-import Link from "next/link"
-import React from "react"
+import Link from "next/link";
+import { auth } from "@clerk/nextjs/server";
+import { Button } from "@/components/ui/button";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
+import QuestionsCard from "@/components/cards/QuestionsCard";
+import { getQuestions } from "@/lib/actions/question.action";
+import { HomePageFilters } from "@/constants/filters";
+import type { SearchParamsProps } from "@/types";
+import type { Metadata } from "next";
 
-export default async function Home() {
+export const metadata: Metadata = {
+  title: "Home — DevStacks",
+};
 
-  const result = await getQuestions({});
+export default async function Home({ searchParams }: SearchParamsProps) {
 
-  console.log(result.questions);
+  const result = await getQuestions({
+      searchQuery: searchParams.q,
+    });
+
   return (
     <>
       <div className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
         <h1 className="h1-bold text-dark100_light900">All Questions</h1>
 
         <Link href="/ask-question" className="flex justify-end max-sm:w-full">
-          <Button className="primary-gradient min-h-[46px] px-4 py-3 text-light-900">
+          <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">
             Ask a Question
           </Button>
         </Link>
       </div>
+
       <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
-        <LocalSearchbar 
+        <LocalSearchbar
           route="/"
           iconPosition="left"
-          placeholder=""
           imgSrc="/assets/icons/search.svg"
+          placeholder="Search for questions"
           otherClasses="flex-1"
-          />
-          <Filter 
-            filters={HomePageFilters}
-            otherClasses="min-h-[56px] sm:min-w-[170px]"
-            containerClasses="hidden max-md:flex"
-            />
+        />
+
+        <Filter
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        />
       </div>
-      <HomeFilters />
 
       <div className="mt-10 flex w-full flex-col gap-6">
-        { 
-          result.questions.length > 0 ? 
-          result.questions.map((question) => (
-            <QuestionsCard 
-            key={question._id}
-            _id={question._id}
-            title={question.title}
-            tags={question.tags}
-            author={question.author}
-            upvotes={question.upvotes}
-            views={question.views}
-            answers={question.answers}
-            createdAt={question.createdAt}
+        {result.questions.length > 0 ? (
+          result.questions.map((question: any) => (
+            <QuestionsCard
+              key={question._id}
+              _id={question._id}
+              clerkId={clerkId}
+              title={question.title}
+              tags={question.tags}
+              author={question.author}
+              upvotes={question.upvotes}
+              views={question.views}
+              answers={question.answers}
+              createdAt={question.createdAt}
             />
-          )) : 
-          <NoResult 
-          title="There's no question to show"
-          description="Be the first to break the silence! Ask a Question 
-            and kickstart the discussion. Our query could be the 
-            next big thing others learn from. Get involved!"
-          link="/ask-question"
-          linkTitle="Ask a Question"
+          ))
+        ) : (
+          <NoResult
+            title="No Questions Found"
+            description="Be the first to break the silence! 🚀 Ask a Question and kickstart the
+          discussion. our query could be the next big thing others learn from. Get
+          involved! 💡"
+            link="/ask-question"
+            linkTitle="Ask a Question"
           />
-        }
+        )}
       </div>
     </>
-  )
+  );
 }
-
